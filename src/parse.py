d6825c9c108cca2dc7742c2f6c115674f85a1874
Sono Willbrand <sono.willbrand@daubit.org>
2023-05-12 10:24:17 +0200

    add bible anki import

--- src/parse.py
+++ src/parse.py
@@ -5,18 +5,19 @@ with open("../data/memorize.txt", "r", encoding="utf8") as file:
 with open("../data/export.csv", "r", encoding="utf8") as file:
     bible = file.read().split("\n")
 
-verse_regex = "[a-zA-Z].{2,5}. [0-9]*,[0-9]*"
+verse_regex = "([0-9]. )?[a-zA-Z].{1,5}. [0-9]*,[0-9]*"
 tag_regex = "([a-zA-Z ] *)+"
 
 verses = memo.split("\n")
+export = ""
 for verse in verses:
     verse = verse.strip()
     if re.fullmatch(verse_regex, verse):
-        result = [b for b in bible if b.startswith(f"{verse}|")]
-        if result == []:
-            print(verse)
-        else:
-            print(result)
+        for (i, content) in enumerate(bible):
+            if content.startswith(f"{verse}|"):
+                bible[i] = f"{content} {tag}"
     elif re.fullmatch(tag_regex, verse):
         tag = verse.replace(" ", "_")
-        print(tag)
+
+with open("../data/export-tags.csv", "x", encoding="utf8") as file:
+    file.write("\n".join(bible))
